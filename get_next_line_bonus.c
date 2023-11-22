6dd1bbfa6621be2ce50f0102ec32e1f79b12c9b0
bda-mota <bea.mota04@gmail.com>
2023-11-22 18:36:40 -0300

    bonus part

---
+++ get_next_line_bonus.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bda-mota <bda-mota@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/22 18:14:21 by bda-mota          #+#    #+#             */
+/*   Updated: 2023/11/22 18:35:00 by bda-mota         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+int	ft_read_file(int fd, t_find *file, t_gnl **root)
+{
+	file->bytes = read(fd, file->buffer, BUFFER_SIZE);
+	if (file->bytes < 0)
+	{
+		ft_dealloc(root);
+		return (0);
+	}
+	if (file->bytes == 0)
+		return (1);
+	file->buffer[file->bytes] = '\0';
+	return (2);
+}
+
+char	*get_next_line(int fd)
+{
+	static t_find	f[1024];
+	t_gnl			*root;
+	int				len;
+	int				verify;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	root = NULL;
+	len = 0;
+	while (ft_lstchr(root) == 0)
+	{
+		if (f[fd].pos > 0 && f[fd].pos != f[fd].bytes && f[fd].bytes != 0)
+		{
+			len += ft_build_line(&f[fd], &root, f[fd].bytes);
+			continue ;
+		}
+		verify = ft_read_file(fd, &f[fd], &root);
+		if (verify == 0)
+			return (NULL);
+		if (verify == 1)
+			break ;
+		len += ft_build_line(&f[fd], &root, f[fd].bytes);
+	}
+	return (ft_transform(root, len));
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bda-mota <bda-mota@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/22 18:14:23 by bda-mota          #+#    #+#             */
+/*   Updated: 2023/11/22 18:19:04 by bda-mota         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+typedef struct s_gnl
+{
+	char			c;
+	struct s_gnl	*next;
+}	t_gnl;
+
+typedef struct s_find
+{
+	char	buffer[BUFFER_SIZE];
+	int		pos;
+	int		bytes;
+}	t_find;
+
+char	*get_next_line(int fd);
+char	*ft_transform(t_gnl *root, int len);
+void	ft_insert_end(t_gnl **root, char c);
+int		ft_build_line(t_find *file, t_gnl **root, int bytes);
+int		ft_read_file(int fd, t_find *file, t_gnl **root);
+int		ft_lstchr(t_gnl *root);
+void	ft_dealloc(t_gnl **root);
+
+#endif
